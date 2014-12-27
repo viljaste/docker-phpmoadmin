022a74a830fdec5d78e477a65e0e981096ff0540
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-27 09:39:28 +0200

    Update

--- src/build/modules/phpmoadmin/manifests/init.pp
+++ src/build/modules/phpmoadmin/manifests/init.pp
@@ -2,8 +2,21 @@ class phpmoadmin {
   require phpmoadmin::php
   require phpmoadmin::httpd
 
-  exec { 'pecl install mongo':
-    timeout => 0,
-    path => ['/usr/bin']
+  exec { '/bin/su - root -c "echo \"no\" | pecl install mongo"':
+    timeout => 0
+  }
+
+  file { '/etc/php5/apache2/conf.d/mongo.ini':
+    ensure => present,
+    source => 'puppet:///modules/phpmoadmin/etc/php5/apache2/conf.d/mongo.ini',
+    mode => 644,
+    require => Exec['/bin/su - root -c "echo \"no\" | pecl install mongo"']
+  }
+
+  file { '/etc/php5/cli/conf.d/mongo.ini':
+    ensure => present,
+    source => 'puppet:///modules/phpmoadmin/etc/php5/cli/conf.d/mongo.ini',
+    mode => 644,
+    require => Exec['/bin/su - root -c "echo \"no\" | pecl install mongo"']
   }
 }
